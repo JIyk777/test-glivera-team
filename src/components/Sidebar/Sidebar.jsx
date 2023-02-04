fbad7609af111b0bf95f04fc87eef6bb2b3beaf0
Buzanov Kostya <buzanov.kostya@gmail.com>
2023-02-04 12:10:29 +0200

    update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import scss from './MainSidebar.module.scss';
-import Logo from './Logo/Logo';
+import Logo from './MainSidebarLogo/Logo';
 
 const MainSidebar = () => {
   return (
